2769ab2b24fc8f70caf51ab228f4d39bc32b30ae
Kai Burton <kai@aniski.com>
2021-07-25 05:52:29 +0100

    my not so secret, secret path

--- src/main/kotlin/spb/git/Shell.kt
+++ src/main/kotlin/spb/git/Shell.kt
@@ -1,12 +1,14 @@
 package spb.git
 
+import spb.Constants
+
 /**
  * @author Kai
  */
 class Shell { //TODO Change this, lot of test code that needs optimizing
 
     var CMD = arrayOf("cmd", "/c"/*, "start", "cmd", "/k"*/)
-    var DIRECTORY = arrayOf("cd \"C:\\Users\\Kai\\IntelliJProjects\\free-socks-proxies-autoupdate\" && ")
+    var DIRECTORY = arrayOf("cd \"${Constants.MY_SECRET_LOCAL_PATH}\" && ")
 
     fun executeShell(gitArguments : Array<String>) {
         println(Runtime.getRuntime().exec(CMD.plus(DIRECTORY).plus(gitArguments)).inputStream.reader().readText())
